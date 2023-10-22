57d83e953265b8dc1a80f5cbe60991b93f09d91a
ZanyeNair <zayne.nair@circuitrunners.com>
2023-10-22 00:15:34 -0400

    1st itteration

--- Task.java
+++ Task.java
@@ -1,6 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task {
-  private int numberOfTries;
-  public Task(int t){
-    numberOfTries = t;
+  private List<List<String>> twoDArrayList = new ArrayList<>();
+  private List<String> mun = new ArrayList<>();
+  private List<String> sun = new ArrayList<>();
+  private List<String> tues = new ArrayList<>();
+  private List<String> wed = new ArrayList<>();
+  private List<String> thur = new ArrayList<>();
+  private List<String> fri = new ArrayList<>();
+  private List<String> sat = new ArrayList<>();
+  private List<String> toDo = new ArrayList<>();
+
+  public Task(){
+    twoDArrayList.add(sun);
+    twoDArrayList.add(mun);
+    twoDArrayList.add(tues);
+    twoDArrayList.add(wed);
+    twoDArrayList.add(thur);
+    twoDArrayList.add(fri);
+    twoDArrayList.add(sat);
+    toDo.add("1. Look at all task");
+    toDo.add("2. Look at task for a day");
+    toDo.add("3. Add task");
+    toDo.add("4. Remove task");
+
   }
+  public void toDos(){
+    for (int i = 0; i < toDo.size(); i++) {
+      System.out.println(toDo.get(i));
+    }
+  }
+  public void allTask() {
+    for (List<String> row : twoDArrayList) {
+      for (String element : row) {
+          System.out.print(row + ": " + element + " ");
+      }
+      System.out.println();
+    }
+  }
+
+    
+
 }
--- taskTester.java
+++ taskTester.java
@@ -1,5 +1,74 @@
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class taskTester{
     public static void main(String[] args) {
-        Task one = new Task(1);
+        boolean repeat = true;
+        Scanner in = new Scanner(System.in);
+        while (repeat){
+            Task mainTask = new Task();
+            System.out.println("What do you want to do: ");
+            mainTask.toDos();
+            int a;
+            boolean c = true;
+            while (c == true) {
+                try{
+                    a = in.nextInt();
+                    while(in.hasNextInt()){
+                        if (a>4){
+                            System.out.println("Use a valid value");
+                        }
+                        else{
+                            c = false;
+                        }
+                    }
+                }
+                catch(Exception e){
+                    System.out.println("Use a valid value");
+                }
+                /* 
+                String b = in.next();
+                if (b == "1"){
+                    a = 1;
+                    c = false;
+                }
+                else if (b == "2"){
+                    a = 2;
+                    c = false;
+                }
+                else if (b == "3"){
+                    a = 3;
+                    c = false;
+                }
+                else if (b == "4"){
+                    a = 4;
+                    c = false;
+                }
+                else {
+                    System.out.println("Please enter a valid number");
+                }
+                */
+            }
+
+
+
+
+            
+            System.out.println("Yes");
+
+            
+            System.out.print("Do you want to continue? (Y/N)");
+            String answer = in.next();
+            if (answer.equals("N") || answer.equals("n")) {
+                repeat = false;
+            }
+
+
+
+
+        }
+        
     }
 }
+
