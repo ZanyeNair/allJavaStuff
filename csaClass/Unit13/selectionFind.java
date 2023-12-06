364af9df10dfe5270788baceb0a34832843a4d9f
ZanyeNair <zayne.nair@circuitrunners.com>
2023-12-06 09:29:51 -0500

    new

--- csaClass/Unit13/selectionFind.java
+++ csaClass/Unit13/selectionFind.java
@@ -1,18 +1,8 @@
 package Unit13;
 
 public class selectionFind {
-    int key = 0;
-    public int sort(int[] list, int find){
 
-        if(key >= list.length-1){
-            return -1;
-        }
-        else if(list[key] == find){
-            return key;
-        }
-        else{
-            key++;
-            return sort(list, find);
-        }
+    public static void main(String[] args) {
+        System.out.println(1);
     }
 }
